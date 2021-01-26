316ca0bfebae243d9e6dac466704d80b24a8815e
vapordev <wagner.dev1337@gmail.com>
2021-01-25 21:43:32 -0300

    a bunch of junk

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+
+    before_action :authenticate_user!, except: [:index, :show]
+
+    def index
+        @posts = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to @post
+        else
+            render "new"
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(params[:post].permit(:title, :body))
+            redirect_to @post
+        else
+            render "edit"
+        end
+    end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		
+		redirect_to root_path
+	end
+    
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+end
